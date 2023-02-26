4ff67fcf779627dd39c7eb467cd2b701b4c5a4af
Valdir Amaral <valdir.amaral58@gmail.com>
2023-02-25 23:30:14 -0300

    ranking page

--- client/src/pages/Ranking.svelte
+++ client/src/pages/Ranking.svelte
@@ -1,29 +1,47 @@
 <script>
+    import _ from 'lodash'
     import Fetch from "../components/Fetch.svelte";
+    import Header from "../components/Header.svelte";
     const clanID = "8a1ead4a-2269-4ae8-95b3-afce4639bd15";
     
  
 </script>
-
-{#if clanID}
+<Header titulo="Voltar"/>
 <h1>
     Ranking de xp
 </h1>
-<Fetch endpoint="/clans/{clanID}/members" let:data>
+
+<Fetch endpoint="clans/{clanID}/members" let:data>
     <ul>
-        {#each data as user}
-            <li>{user.username} xp: {user.xp}</li>
+        {#each _.orderBy(data, ['user', 'xp'], ['desc', 'desc'])  as user}
+            <li><span style="background-color: {user.profileIconColor};"></span>{user.username} ➙ {user.xp}xp</li>
         {/each}
     </ul>
 </Fetch>
-{:else}
-<h1>Busque o seu clan!</h1>
-{/if}
+
 
 <style>
     h1 {
         text-align: center;
         padding: 30px;
     }
-   
+    span {
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        display: block;
+    }
+    ul {
+        width: 80%;
+        margin: auto;
+        max-width: 400px;
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+    }
+    li {
+        display: flex;
+        gap: 20px;
+        align-items: center;
+    }
 </style>
