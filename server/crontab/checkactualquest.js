759de998e2eb044a4beb0abb710995a23cad2d3f
Valdir Amaral <valdir.amaral58@gmail.com>
2023-03-12 17:42:34 -0300

    adding top 3 xp participant on game chat

--- server/crontab/checkactualquest.js
+++ server/crontab/checkactualquest.js
@@ -7,7 +7,6 @@ let clanID = '8a1ead4a-2269-4ae8-95b3-afce4639bd15';
 
 var cont = 0
 async function checkActualQuest() {
-    console.log('Checado')
     fetch(`https://api.wolvesville.com/clans/${clanID}/quests/active`, {
         method: 'GET',
         headers: {
@@ -20,24 +19,26 @@ async function checkActualQuest() {
         return res.json()
     })
     .then((json) => {
-        console.log(json.tierFinished)
+        console.log('Checado')
+        console.log(`terminou? ${json.tierFinished ? "Sim" : "Não"}`)
         let participants = json.participants
         participants.sort(function(a,b) {
             return b.xp - a.xp
         })
-        let mensagem = `Uma etapa da missão foi concluída, obrigado pela participação de todos!\n\nClassificação:\n`
+        let mensagem = `Uma etapa da missão foi concluída, obrigado pela participação de todos!\nTop 3:\n`
         participants.forEach((p, index) => {
-            mensagem += `${index + 1}º ${p.username}: ${p.xp}xp \n`
+            if(index < 3) {
+                mensagem += `${index + 1}º ${p.username}: ${p.xp}xp\n`
+            }
+            
         })
-        mensagem += "\n*Lembrando que o primeiro lugar pode escolher ganhar 10 rosas ou não pagar a próxima missão!"
+        mensagem += "\n*Lembrando que o primeiro lugar pode escolher ganhar 10 rosas ou não pagar a próxima missão!\n"
         if(!json.quest) return // se não existir missões
-
+  
         if(json.tierFinished) { // se a nível da missão foi concluído
             // pula para a próxima etapa
-            console.log('Nível finalizado')
-            cont++
-            if (cont != 1) return 
-            /*fetch(`https://api.wolvesville.com/clans/${clanID}/quests/active/skipWaitingTime`, {
+            console.log('mensagem enviada')
+            fetch(`https://api.wolvesville.com/clans/${clanID}/quests/active/skipWaitingTime`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -45,17 +46,21 @@ async function checkActualQuest() {
                     'Authorization': `Bot ${botID}`
                 }
             })
-            */
+            
             // manda uma mensagem de agradecimento
             fetch(`https://api.wolvesville.com/clans/${clanID}/chat`, {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                    'Authorization': `Bot ${botID}`
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Authorization": `Bot ${botID}`
                 },
                 body: JSON.stringify({"message": mensagem})
             })
+            .then((response) => {
+                return response.json()
+            })
+            
         }
     })
 }
